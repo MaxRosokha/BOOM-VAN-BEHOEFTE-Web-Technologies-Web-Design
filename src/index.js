b09e05df489b7fa7dd536029acd594531f52dd89
Max Rosokha <maks.rosokha.1978@gmail.com>
2024-10-03 10:53:08 +0300

    Start of layout of the first screen

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-import '/bootstrap';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+import App from './App';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />  {/* App */}
+    </React.StrictMode>,
+    document.getElementById('root')
+);
