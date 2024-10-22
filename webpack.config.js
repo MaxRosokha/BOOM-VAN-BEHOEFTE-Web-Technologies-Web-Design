08dafa13d4301db5312422a9a687ada7cf79b8b4
Max Rosokha <maks.rosokha.1978@gmail.com>
2024-10-22 14:31:57 +0300

    Сhanging webpack, conducting tests

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,42 @@
-/* eslint-disable no-undef */
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
-module.exports = (env, argv) => {
-  const isDevelopment = argv.mode === 'development';
+module.exports = (env, argv = {}) => {
+  const isDevelopment = argv.mode === 'development' || process.env.NODE_ENV === 'development';
 
   return {
     mode: isDevelopment ? 'development' : 'production',
     entry: './src/index.js',
     output: {
-      filename: 'dnl_periodswebresource.js',
+      filename: 'bundle.js',
       path: path.resolve(__dirname, 'dist'),
-      clean: true,  // очищує папку dist перед кожною збіркою
     },
     module: {
       rules: [
         {
-          test: /\.(js|jsx)$/, // Додано підтримку .jsx
+          test: /\.(js|jsx|ts|tsx)$/,
           exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env', '@babel/preset-react'], // Додано @babel/preset-react
-            },
-          },
-        },
-        {
-          test: /\.(png|jpe?g|gif|svg)$/i,
-          exclude: /node_modules/,
-          use: {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[hash].[ext]', // Додає хеш для уникнення кешування
-              outputPath: 'images',        // Зберігає картинки в окрему папку
-            },
-          },
+          use: 'babel-loader',
         },
         {
           test: /\.css$/,
           use: ['style-loader', 'css-loader'],
         },
+        {
+          test: /\.(png|jpg|gif|svg|woff|woff2|eot|ttf|otf)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[hash].[ext]',
+                outputPath: 'assets',
+              },
+            },
+          ],
+        },
       ],
     },
     resolve: {
-      extensions: ['.js', '.jsx'], // Додано підтримку JSX
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './public/index.html', // Задає шлях до шаблону HTML
-      }),
-    ],
-    devServer: {
-      static: {
-        directory: path.join(__dirname, 'dist'),
-      },
-      hot: true,
-      open: true,
-    },
-    optimization: {
-      minimize: !isDevelopment, // Оптимізація в production режимі
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
     },
   };
 };
