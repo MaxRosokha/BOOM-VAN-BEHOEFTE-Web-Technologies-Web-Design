1b02fad866ad41f8e0cb41944946b438ed82be15
Max Rosokha <maks.rosokha.1978@gmail.com>
2024-11-03 14:05:33 +0200

    Update

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style.css';
-import App from './App';
 import { DevSupport } from '@react-buddy/ide-toolbox';
 import { ComponentPreviews, useInitial } from './src/dev';
+import App from './src/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
